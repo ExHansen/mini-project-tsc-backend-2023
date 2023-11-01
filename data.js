ccc9f86cb4e677ea89cde5936be43f679037f0dd
ExHansen <91862289+ExHansen@users.noreply.github.com>
2023-11-01 18:40:51 +0700

    data.js

--- data.js
+++ data.js
@@ -1,9 +1,7 @@
-// data.js
 const libraryData = [
-    { title: "Buku 1", author: "Penulis 1" },
-    { title: "Buku 2", author: "Penulis 2" },
-    { title: "Buku 3", author: "Penulis 3" },
-    // Tambahkan data buku lainnya sesuai kebutuhan
+    { title: "Sherlock Holmes", author: "Michael" },
+    { title: "Detective Conan", author: "Felix" },
+    { title: "Naruto", author: "Sasuke" },
 ];
 
-module.exports = libraryData;
+export default libraryData;
