9f1f87d1edf820f5fb3d1f5dc72acf22f6fee5c7
ExHansen <91862289+ExHansen@users.noreply.github.com>
2023-11-01 18:40:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,117 @@
-// app.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const libraryData = require('./data'); // Impor data dari data.js
-const app = express();
+import http from 'http';
+import data from './data.js';
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const requestListener = (req, res) => {
+    const method = req['method'];
+    const url = req.url.split('/')[1];
+    const params = req.url.split('/')[2];
+    
+    res.setHeader('Content-Type', 'application/json');
 
-// Endpoint untuk menambahkan buku
-app.get('/hello', (req, res) => {
-    res.json('Hello World');
- })
+    if(method == 'GET' && req.url == '/getBooks'){
+        const responseJSON = {
+            message:"Data berhasil didapatkan",
+            data: data,
+        };
+            res.end(JSON.stringify(responseJSON));
+    }
 
+    if (method === 'POST' && req.url === '/addBook') {
+        let requestBody = '';
+        req.on('data', (data) => {
+            requestBody += data;
+            console.log(requestBody);
+        });
+    
+        req.on('end', () => {
+            try {
+                requestBody = JSON.parse(requestBody);
+                data.push(requestBody);
+                const responseJSON = {
+                    message: "Data berhasil diinputkan",
+                    data: data,
+                };
+                res.end(JSON.stringify(responseJSON));
+            } catch (error) {
+                const responseJSON = {
+                    message: "Gagal memproses data",
+                };
+                res.statusCode = 400; 
+                res.end(JSON.stringify(responseJSON));
+            }
+        });
+    }       
 
-const port = process.env.PORT || 3000;
+    if (method === 'PUT' && req.url.startsWith('/editBook/')) {
+        const urlParts = req.url.split('/');
+        const index = urlParts[2]; 
+        const bookIndex = parseInt(index, 10); 
+        let requestBody = '';
+
+        req.on('data', (data) => {
+            requestBody += data;
+        });
+
+        req.on('end', () => {
+            try {
+                requestBody = JSON.parse(requestBody);
+
+                if (bookIndex < 0 || bookIndex >= libraryData.length) {
+                    res.statusCode = 404;
+                    return res.end(JSON.stringify({
+                        message: 'Data tidak ditemukan',
+                    }));
+                } else {
+                    libraryData[bookIndex] = {
+                        title: requestBody.title,
+                        author: requestBody.author,
+                    };
+
+                    res.statusCode = 200;
+                    return res.end(JSON.stringify({
+                        message: 'Data berhasil diperbaharui',
+                    }));
+                }
+            } catch (error) {
+                res.statusCode = 400;
+                return res.end(JSON.stringify({
+                    message: 'Gagal memproses data',
+                }));
+            }
+        });
+    }
+
+    if(method == 'DELETE'){
+        switch('/deleteBook'){
+            case 'libraryData':
+                const index = data.findIndex((data) => {
+                    data.index == params;
+                });
+
+                data.splice(index, 1);
+                if(index < 0){
+                    res.statusCode = 404;
+                    return res.end(
+                        JSON.stringify({
+                            message: 'data tidak ditemukan',
+                        })
+                    );
+                }else{
+                    res.statusCode = 200;
+                    return res.end(
+                        JSON.stringify({
+                            message: 'data berhasil dihapus',
+                        })
+                    )
+                }
+        }
+    } 
+};
+
+const app = http.createServer(requestListener);
+
+const port = 3000;
+
 app.listen(port, () => {
-    console.log(`Server berjalan di port ${port}`);
-});
+    console.log(`Console running on port ${port}`);
+})
